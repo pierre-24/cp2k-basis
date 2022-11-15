5164d7a2f396a3d60679cefc0385198a38367650
Pierre Beaujean <pierreb24@gmail.com>
2022-11-15 11:15:40 +0100

    add base parser

--- cp2k_basis/parser.py
+++ cp2k_basis/parser.py
@@ -1,4 +1,4 @@
-from typing import Iterator, Callable
+from typing import Iterator, Callable, List, Union
 from enum import Enum, unique
 
 
@@ -60,5 +60,101 @@ class Lexer:
         yield Token(TokenType.EOS, '\0', self.position)
 
 
+class ParserSyntaxError(Exception):
+    pass
+
+
 class ParserNode:
     pass
+
+
+class BaseParser:
+    def __init__(self, inp: str):
+        self.lexer = Lexer(inp)
+        self.tokenizer = self.lexer.tokenize()
+        self.current_token: Token = None
+
+        self.next()
+
+    def next(self):
+        """Get next token"""
+
+        try:
+            self.current_token = next(self.tokenizer)
+        except StopIteration:
+            self.current_token = Token(TokenType.EOS, '\0')
+
+    def eat(self, typ: TokenType):
+        if self.current_token.type == typ:
+            self.next()
+        else:
+            raise ParserSyntaxError('expected {}, got {}'.format(typ, self.current_token))
+
+    def comment(self) -> None:
+        """
+        COMMENT := '#' (WORD | SPACE)* NL?
+        """
+
+        if self.current_token.type != TokenType.WORD or self.current_token.value[0] != '#':
+            raise ParserSyntaxError('expected WORD starting with `#` for COMMENT')
+
+        self.eat(TokenType.WORD)
+        while self.current_token.type not in [TokenType.NL, TokenType.EOS]:
+            self.next()
+
+        if self.current_token.type == TokenType.NL:
+            self.next()
+
+    def integer(self) -> int:
+        """Parse integer
+        """
+        if self.current_token.type != TokenType.WORD:
+            raise ParserSyntaxError('expected WORD for integer, got {}'.format(self.current_token.type))
+
+        try:
+            number = int(self.current_token.value)
+        except ValueError:
+            raise ParserSyntaxError('expected integer, got {}'.format(self.current_token.value))
+
+        self.next()
+        return number
+
+    def number(self) -> float:
+        """Parser float
+        """
+
+        if self.current_token.type != TokenType.WORD:
+            raise ParserSyntaxError('expected WORD for number, got {}'.format(self.current_token.type))
+
+        try:
+            number = float(self.current_token.value)
+        except ValueError:
+            raise ParserSyntaxError('expected number, got {}'.format(self.current_token.value))
+
+        self.next()
+        return number
+
+    def line(self, definition: str) -> List[Union[int, float, str]]:
+        """
+        LINE := (integer | number | WORD)* NL
+        """
+        result = []
+
+        for d in definition:
+            if d == 'i':
+                result.append(self.integer())
+            elif d == 'n':
+                result.append(self.number())
+            elif d == 'w':
+                if self.current_token.type is not TokenType.WORD:
+                    raise ParserSyntaxError('expected WORD in LINE, got {}'.format(self.current_token.type))
+                result.append(self.current_token.value)
+                self.next()
+
+            if len(result) < len(definition):
+                self.eat(TokenType.SPACE)
+
+        if self.current_token.type != TokenType.EOS:
+            self.eat(TokenType.NL)
+
+        return result
--- tests/tests_parser.py
+++ tests/tests_parser.py
@@ -1,6 +1,6 @@
 import unittest
 
-from cp2k_basis.parser import Lexer, Token as TK, TokenType as TT
+from cp2k_basis.parser import Lexer, Token as TK, TokenType as TT, BaseParser, ParserSyntaxError
 
 
 class LexerTestCase(unittest.TestCase):
@@ -21,3 +21,36 @@ class LexerTestCase(unittest.TestCase):
         for i, token in enumerate(Lexer('ceci est\nun test ').tokenize()):
             self.assertEqual(token.type, expected[i].type)
             self.assertEqual(token.value, expected[i].value)
+
+
+class BaseParserTestCase(unittest.TestCase):
+    def test_numbers_ok(self):
+        self.assertEqual(BaseParser('42').integer(), 42)
+        self.assertEqual(BaseParser('42.25').number(), 42.25)
+
+    def test_not_numbers_ko(self):
+        with self.assertRaises(ParserSyntaxError):
+            BaseParser('a').integer()
+
+        with self.assertRaises(ParserSyntaxError):
+            BaseParser('42a').integer()
+
+    def test_comment_ok(self):
+        parser = BaseParser('# tmp\n42')
+
+        parser.comment()  # skip comment entirely
+        self.assertEqual(parser.integer(), 42)
+
+    def test_line_ok(self):
+        expected = [1, 'a', 2.32]
+        self.assertEqual(BaseParser(' '.join('{}'.format(e) for e in expected)).line('iwn'), expected)
+
+    def test_wrong_line_ko(self):
+        with self.assertRaises(ParserSyntaxError):  # too short
+            BaseParser('42 a').line('i')
+
+        with self.assertRaises(ParserSyntaxError):  # incorrect
+            BaseParser('42 a').line('wi')
+
+        with self.assertRaises(ParserSyntaxError):  # too long
+            BaseParser('42 a').line('iww')
