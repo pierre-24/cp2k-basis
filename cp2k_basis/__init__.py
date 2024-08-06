bfa4aecd67d3878a21897f9c69225543ac105ca5
Pierre Beaujean <pierreb24@gmail.com>
2024-08-06 16:18:36 +0200

    Bump version: 0.5.1 → 0.5.2

--- cp2k_basis/__init__.py
+++ cp2k_basis/__init__.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 __author__ = 'Pierre Beaujean'
 __maintainer__ = 'Pierre Beaujean'
 __email__ = 'pierre.beaujean@unamur.be'
